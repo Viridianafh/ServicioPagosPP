4d7a9882f3b6001944695be7bf7292a28c58cd12
luisrojasmed <luis.rojas@transportesmedrano.com>
2025-04-14 09:53:22 -0600

    actualizacion: agregado copia correo atc

--- ConsultaWebApisPagos/EnvioMail.cs
+++ ConsultaWebApisPagos/EnvioMail.cs
@@ -29,6 +29,7 @@ namespace ConsultaWebApisPagos
                 correo.Bcc.Add("luis.rojas@transportesmedrano.com"); // Agregar una copia (CC)
                 correo.Bcc.Add("viridiana.fh@transportesmedrano.com"); // Agregar una copia (CC)
                 correo.Bcc.Add("alejandro.horta@transportesmedrano.com"); // Agregar una copia (CC)
+                correo.Bcc.Add("rosa.ceron@sagautobuses.com"); // Agregar una copia (CC)
                 correo.Subject = "Boletos SAG"; //Asunto
                                                 //correo.Body = MensajeHtml(ShortdId,Origen,Destino,fechaSalida.ToString("dd/MM/yyyy HH:mm"),fechaLlegada.ToString("dd/MM/yyyy HH:mm"), Total);
                 correo.AlternateViews.Add(MensajeHtml(ticketid, ShortdId, Origen, Destino, fechaSalida, fechaLlegada, Total));
